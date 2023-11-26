244a10aae77266e704ac8663eedbc1d0162abbc2
Nick Mugo <nicknjuru13@gmail.com>
2023-11-26 16:09:19 +0300

    Completed assignment but its saying their is a problem and i cant see it but to me its done

--- server/app.py
+++ server/app.py
@@ -1,34 +1,28 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
-from flask_migrate import Migrate
-
-from models import db, Zookeeper, Enclosure, Animal
+from flask import Flask, render_template
+from server.models import db, Animal, Zookeeper, Enclosure
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return '<h1>Zoo app</h1>'
+# Define the models (Animal, Zookeeper, Enclosure) as described in your application
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    return render_template('enclosure.html', enclosure=enclosure)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,47 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy(app)
+
+animal_enclosure = db.Table(
+    'animal_enclosure',
+    db.Column('animal_id', db.Integer, db.ForeignKey('animals.id')),
+    db.Column('enclosure_id', db.Integer, db.ForeignKey('enclosures.id'))
+)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.DateTime)
+
+    # Define the relationship with a different backref name
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    
+    # Define the relationship
+    animals = db.relationship('Animal', secondary=animal_enclosure, back_populates='enclosures')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    enclosure = db.Column(db.Boolean, default=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    
+
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosures = db.relationship('Enclosure', secondary=animal_enclosure, back_populates='animals')
